9e751694bc9a8aed9cfc4af516fe780d11b3cb18
max.chen <amingccc@gmail.com>
2018-12-26 22:06:26 +0800

    修改antd版本

--- bike-manage/src/App.js
+++ bike-manage/src/App.js
@@ -6,8 +6,8 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-          {this.props.children}
+      <div>
+        {this.props.children}
       </div>
     );
   }
--- bike-manage/src/router.js
+++ bike-manage/src/router.js
@@ -1,17 +1,23 @@
 import React from 'react'
+import {HashRouter, Route, Switch} from 'react-router-dom';
 import App from './App'
 import Login from './pages/login'
-import Admin from './pages/login'
-import {HashRouter, Route, Switch} from 'react-router-dom';
+import Admin from './admin'
+
 
 export default class IRouter extends React.Component {
     render() {
         return (
             <HashRouter>
                 <App>
-                    <Route path="/login" component={Login}> </Route>
-                    <Route path="/admin" component={Admin}> </Route>
+                    <Route path="/login" component={Login}/>
+                    <Route path="/admin" render={()=>
+                        <Admin>
+                            <Route path="/admin/ui/buttons" component={Login}/>
 
+                        </Admin>
+                    }/>
+                    <Route path="/order/detail" component={Admin}/>
                 </App>
             </HashRouter>
         );
